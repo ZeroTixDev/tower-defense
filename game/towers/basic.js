e19640749e6ec30ee2e4af8df94d26bfec5907a5
haha0201 <47706454+haha0201@users.noreply.github.com>
2020-12-29 00:52:49 -0500

    ok

--- game/towers/basic.js
+++ game/towers/basic.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const generatePath = (path) => {
+   //Path is in the form of [{x: 0, y: 0}, {x: 1600, y: 900}]
+   return "ZeroTix Pro";
+}
 module.exports = class Tower {
    constructor(path) {}
 };
