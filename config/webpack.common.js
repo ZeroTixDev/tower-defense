f4a46690e71c58cb737e935af7dc00325df4795e
ZeroTix <weddnremy@gmail.com>
2021-01-01 22:48:03 -0500

    trying to put editor and game into different folders, failed but will try tmrw, pushing for now

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const path = require('path');
 const paths = require('./paths');
 const WebpackBar = require('webpackbar');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -16,17 +15,31 @@ const plugins = [
          collapseWhitespace: true,
       },
    }),
+   new HtmlWebpackPlugin({
+      hash: true,
+      title: 'lol',
+      template: paths.editor + '/template.html',
+      filename: './editor/index.html',
+      minify: {
+         removeComments: true,
+         collapseWhitespace: true,
+      },
+   }),
    new CleanWebpackPlugin(),
    new WebpackBar(),
 ];
 module.exports = {
-   entry: [paths.game + '/index.js'],
+   /* entry: {
+      game: { import: paths.game + '/index.js', filename: 'game/[name].[contenthash].bundle.js' },
+      editor: { import: paths.editor + '/index.js', filename: 'editor/[name].[contenthash].bundle.js' },
+   },*/
+   entry: paths.game + '/index.js',
    mode: 'development',
    devtool: 'source-map',
    output: {
-      filename: '[name].[contenthash].js',
       path: paths.build,
       publicPath: '/',
+      filename: '[name].[contenthash].bundle.js',
    },
    module: {
       rules: [
