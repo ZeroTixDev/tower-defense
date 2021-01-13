7bec68e32095387bdb2ac79850f96c5eeb5deae0
ZeroTix <weddnremy@gmail.com>
2021-01-13 12:49:39 -0500

    why is it not working >:C

--- src/game/index.js
+++ src/game/index.js
@@ -6,8 +6,3 @@ const Game = require('./game');
 const game = new Game();
 
 game.start();
-
-const shortcut_icon = document.createElement('link');
-shortcut_icon.rel = 'shortcut icon';
-shortcut_icon.href = 'https://zerotixdev.github.io/tower-defense/dist/game/logo.png';
-document.head.appendChild(shortcut_icon);
