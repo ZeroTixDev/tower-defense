14aa7345f5b19ab801a7994e308e3b4959229346
ZeroTix <weddnremy@gmail.com>
2021-01-13 12:39:51 -0500

    still playing aroundnd with meta tags and sht

--- src/game/index.js
+++ src/game/index.js
@@ -2,6 +2,12 @@
 
 require('./style.css');
 const Game = require('./game');
+
 const game = new Game();
 
 game.start();
+
+const shortcut_icon = document.createElement('link');
+shortcut_icon.rel = 'shortcut icon';
+shortcut_icon.href = 'https://zerotixdev.github.io/tower-defense/dist/game/logo.png';
+document.head.appendChild(shortcut_icon);
