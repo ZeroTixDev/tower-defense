51a09364577fd31e67962f294c14f6e5945b3194
ZeroTixDev <weddnremy@gmail.com>
2021-01-23 00:21:06 -0500

    small changes

--- src/util/revertVersion.js
+++ src/util/revertVersion.js
@@ -5,4 +5,5 @@ const fetch = require('node-fetch');
 
 void fetch('https://version-api.zerotixdev.repl.co/' + process.env.REVERT)
    .then((res) => res.json())
-   .then((val) => console.log(`New Version: ${val?.version}`));
+   .then((val) => console.log(`New Version: ${val?.version}`))
+   .catch(console.log);
